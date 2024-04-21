72f6bd3de5ab2b79c2a9d191443f8677abc4a844
ZWF <2360690156@qq.com>
2024-04-21 21:32:05 +0800

    修改项目名称

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <yu-terminal
+    ref="terminalRef"
+    :user="loginUser"
+    full-screen
+    :on-submit-command="onSubmitCommand"
+  />
+</template>
+
+<script setup lang="ts">
+import { doCommandExecute } from "../core/commandExecutor";
+import { onMounted, ref } from "vue";
+import { useUserStore } from "../core/commands/user/userStore";
+import { storeToRefs } from "pinia";
+
+const terminalRef = ref();
+
+/**
+ * @param inputText 输入的命令
+ */
+const onSubmitCommand = async (inputText: string) => {
+  if (!inputText) {
+    return;
+  }
+  const terminal = terminalRef.value.terminal;
+  // 解析命令
+  await doCommandExecute(inputText, terminal);
+};
+
+// 获取用户信息
+const userStore = useUserStore();
+const { loginUser } = storeToRefs(userStore);
+
+onMounted(() => {
+  userStore.getAndSetLoginUser();
+});
+</script>
+
+<style></style>
